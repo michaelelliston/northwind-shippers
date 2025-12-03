147b32f85cca00c97818cc75e0bd50569b29bfff
Michael <michael.elliston95@gmail.com>
2025-12-03 11:50:18 -0600

    Created a Shipper DTO class to hold data for a shipper

--- src/main/java/Shipper.java
+++ src/main/java/Shipper.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Shipper {
+
+    private final long shipperId;
+    private final String shipperName;
+    private final String phoneNumber;
+
+    public Shipper(long shipperId, String shipperName, String phoneNumber) {
+        this.shipperName = shipperName;
+        this.phoneNumber = phoneNumber;
+        this.shipperId = shipperId;
+    }
+
+    public String getShipperName() {
+        return shipperName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public long getShipperId() {
+        return shipperId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Shipper ID: %d | Shipper Name: %s | Shipper Phone Number: %s", this.getShipperId(), this.getShipperName(), this.getPhoneNumber());
+    }
 }
